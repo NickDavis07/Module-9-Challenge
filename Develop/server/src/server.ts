d38fcf764ae902aabd773bdda685e6d749c60402
Nick Davis <nickwagner@live.com>
2024-12-17 16:27:12 -0700

    completed server.ts file

--- Develop/server/src/server.ts
+++ Develop/server/src/server.ts
@@ -9,11 +9,14 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-// TODO: Serve static files of entire client dist folder
+// TODO: Serve static files of entire client dist folder - Done
+app.use(express.static('../client/dist'));
 
-// TODO: Implement middleware for parsing JSON and urlencoded form data
+// TODO: Implement middleware for parsing JSON and urlencoded form data - Done
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// TODO: Implement middleware to connect the routes
+// TODO: Implement middleware to connect the routes - Done
 app.use(routes);
 
 // Start the server on the port
